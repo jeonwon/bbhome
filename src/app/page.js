32588d1299e21eb86a5bc2901579e47cd8c45f82
(전원식) <(jeonwons@hanmail.net)>
2024-11-15 12:34:05 +0900

    버셀에 올리기위한작업

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Get started by editing&nbsp; , BoBae</h1>
+      <h1>여기는 page.js파일 위치임.... BoBae</h1>
 
-      <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+      <p>
+        Instantly deploy your Next.js site to a shareable URL with Vercel.n\
+        버셀에 올려봅니다
+      </p>
     </main>
   );
 }

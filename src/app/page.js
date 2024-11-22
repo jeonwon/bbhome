539678389e1df3d6694eb838be71448bab964b84
(전원식) <(jeonwons@hanmail.net)>
2024-11-22 12:34:36 +0900

    new design of portfolio for jeon

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,19 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>여기는 page.js파일 위치임.... BoBae</h1>
+      <div className="text-2xl mb-5 ml-4">안녕하세요? 田寶培입니다~</div>
 
-      <p>
-        Instantly deploy your Next.js site to a shareable URL with Vercel.
-        <br />
-        버셀에 올려봅니다
+      <p className="text-xl p-4">
+        제가 태어난 곳은 경기도 파주시 파평면 율곡리입니다. 율곡 이이선생님께서
+        작시와 묵상을 하시던 곳이고 선생님의 8세시가 새겨있습니다. 멀리 개성의
+        송악산이 어렴풋이 보이고 굽이쳐 흐르는 임진강이 아래로 내려다 보이는곳
+        화석정이 이라는 동네 입니다. 화석정의 바로 아래로는 자유로가 놓여져 있고
+        그 아래로 임진강이 있는데 강가에는 분단의 상징인 철책이 놓여져 있습니다.
+        제가 어릴때에는 미군들이 쓰다버린 나즈막하고 둥근모양의 철조 망이 두문
+        두문 있었고 그 철조망을 밟고 임진강으로 들어가 수영을 하곤 하였습니다.
+        한겨울에는 어름이 두껍게 얼어 임진강에서 얼음을 지치기도 하였고 건너편
+        동파리에 가서 철재 포탄 파편을 주워 오곤 하였습니다. 지금은 어림없는
+        옛날 이야기가 되었습니다만...
       </p>
     </main>
   );
---
+++ src/app/photo/page.js
@@ -0,0 +1,9 @@
+export default function Photo() {
+  return (
+    <>
+      <div className="text-center flex justify-center">
+        <img className="w-1/4 rounded-full" src="./../jbobae.png" />
+      </div>
+    </>
+  );
+}
